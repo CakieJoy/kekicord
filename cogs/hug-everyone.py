fe70813f600aa4e8c57b9a7c11982bdb5bab58dd
CakieJoy <cakiejoy@utkan.net>
2026-09-05 00:41:27 +0300

    fix: needs 1 argument

--- cogs/hug-everyone.py
+++ cogs/hug-everyone.py
@@ -7,7 +7,7 @@ class HugEveryone(commands.Cog):
         self.bot = bot
 
     @app_commands.command(name="hug-everyone", description="Hugs to everyone :3")
-    async def hug_everyone(interaction: discord.Interaction):
+    async def hug_everyone(self, interaction: discord.Interaction):
         display_name = interaction.user.display_name
         embed = discord.Embed(title=f"{display_name} hugs everyone :3",)
         embed.set_image(url="https://c.tenor.com/SYsRdiK-T7gAAAAC/tenor.gif")
