338e7c5aa86173c414b8a011e29f8d5832784ea0
CakieJoy <139656629+CakieJoy@users.noreply.github.com>
2026-09-05 00:43:09 +0300

    Add cog (#4)
    * add cog
    
    * add cog template
    
    * chore: add __pycache__ to gitignore
    
    * fix: needs 1 argument
    
    * fix: needs 1 argument on template

---
+++ cogs/template.py
@@ -0,0 +1,17 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+class commandName(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="command-name", description="Description of the command")
+    async def command_name(self, interaction: discord.Interaction):
+        pass
+        # * codes of your command
+
+async def setup(bot):
+    await bot.add_cog(commandName(bot))
+
+# * to load here 'await self.load_extension("cogs.command-file")' in main.py setup hook
